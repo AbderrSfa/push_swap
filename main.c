35fd93100f101863e99da32113060985861e1cbc
Abderrahmane Sfaihi <abderrahmanesfaihi@hotmail.com>
2021-07-15 14:25:34 +0100

    Norminette for main.c

--- main.c
+++ main.c
@@ -6,21 +6,11 @@
 /*   By: asfaihi <asfaihi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/12 16:27:35 by asfaihi           #+#    #+#             */
-/*   Updated: 2021/07/15 13:09:58 by asfaihi          ###   ########.fr       */
+/*   Updated: 2021/07/15 14:19:01 by asfaihi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
-
-void	print_stack(t_stacks *stacks)
-{
-	int	i;
-
-	i = 0;
-	printf("/**************************stack_a*************************/\n");
-	while (i < stacks->stack_a.in_use)
-		printf("|stack_a size = %d|\t|size used = %d|\t|num = %d|\n",stacks->stack_a.size, stacks->stack_a.in_use,  stacks->stack_a.array[i++]);
-}
 
 int	main(int argc, char **argv)
 {
@@ -45,8 +35,5 @@ int	main(int argc, char **argv)
 		sort_one_hundred(&stacks);
 	else if (stacks.stack_a.size > 100)
 		sort_five_hundred(&stacks);
-	////
-	//print_stack(&stacks);
-	////
 	return (end_program(0, NULL, &stacks));
 }
